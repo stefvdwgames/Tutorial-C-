d49ba79f5943e10f303ef809594a48bd5f077b32
stefvdwgames <stefvdw.games@gmail.com>
2020-06-28 18:09:32 +0200

    Getting Started

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tutorial_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
